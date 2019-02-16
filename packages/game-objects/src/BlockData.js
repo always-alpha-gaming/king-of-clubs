7119f13ca293ebabe3980a44320d08ebe8193805
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-02-16 17:45:52 -0500

    Added BlockType to BlockData

--- packages/game-objects/src/BlockData.js
+++ packages/game-objects/src/BlockData.js
@@ -1,9 +1,10 @@
 class BlockData {
-  constructor(id, position, color, health) {
+  constructor(id, position, color, health, blockType) {
     this.id = id;
     this.position = position;
     this.health = health;
     this.color = color;
+    this.blockType = blockType;
   }
 
   update() {}

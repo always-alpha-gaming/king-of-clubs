4d7710a6c5a857e1dc700250d061e93a50ba5acb
Patrick Gingras <775.pg.12@gmail.com>
2019-02-16 15:38:45 -0500

    change static file port to 1234 for parcel dev server

--- packages/proxy/index.js
+++ packages/proxy/index.js
@@ -1,6 +1,6 @@
 const proxy = require('express-http-proxy');
 const app = require('express')();
 
-app.use('/', proxy('localhost:3000'));
+app.use('/', proxy('localhost:1234'));
 
 app.listen(2000);

ae4dacd479e50b8d00a5468f69114420e3ff0342
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-02-16 17:49:05 -0500

    Added ChunkData and MapData

---
+++ packages/game-objects/src/BlockChunkData.js
@@ -0,0 +1,12 @@
+class BlockChunkData {
+    constructor(id, position, blocks) {
+      this.id = id;
+      this.position = position;
+      this.blocks = blocks;
+    }
+  
+    update() {}
+  }
+  
+  module.exports = BlockChunkData;
+  
---
+++ packages/game-objects/src/MapData.js
@@ -0,0 +1,12 @@
+class MapData {
+    constructor(blockChunks, perlinSeed, borderZ) {
+      this.blockChunks = blockChunks;
+      this.perlinSeed = perlinSeed;
+      this.borderZ = borderZ;
+    }
+  
+    update() {}
+  }
+  
+  module.exports = MapData;
+  

036b89de714cc50372c97cb22b2a8e6c767b352a
macdja38 <jakeincanada@icloud.com>
2019-02-16 18:34:28 -0500

    Complete the chunk generation code

--- packages/server/chunk-gen.js
+++ packages/server/chunk-gen.js
@@ -1,5 +1,7 @@
 const CONFIG = require('config');
 const SimplexNoise = require('simplex-noise');
+const { BlockData, BlockChunkData } = require('game-objects');
+const baseHeight = 64;
 
 /**
  * Takes a chunk coordinate and generates a chunk, (chunk coordinates are not absolute coordinates)
@@ -7,18 +9,35 @@ const SimplexNoise = require('simplex-noise');
  * @param {number} z
  * @param {string} [seed]
  */
-function genChunk(x, z, seed='seed') {
+function genChunk(x, z, seed = 'seed') {
   const simplex = new SimplexNoise(seed);
 
-  const map = new Array(CONFIG.CHUNK_SIZE)
+  const blocks = new Array(CONFIG.CHUNK_SIZE)
     .fill(0)
-    .map(() => new Array(CONFIG.MAP_SIZE)
+    .map(() => new Array(CONFIG.MAP_HEIGHT)
       .fill(0)
-      .map(() => new Array(CONFIG.CHUNK_SIZE)));
+      .map(() => new Array(CONFIG.CHUNK_SIZE)
+        .fill(null)));
 
-  for (let gx = 0; gx < CONFIG.CHUNK_SIZE; gx++) {
-    for (let zx = 0; zx < CONFIG.CHUNK_SIZE; zx++) {
+  for (let relativeX = 0; relativeX < CONFIG.CHUNK_SIZE; relativeX++) {
+    const absoluteX = (x * CONFIG.CHUNK_SIZE) + relativeX;
+    for (let relativeZ = 0; relativeZ < CONFIG.CHUNK_SIZE; relativeZ++) {
+      const absoluteZ = (z * CONFIG.CHUNK_SIZE) + relativeZ;
+      const height = baseHeight + (10 * simplex.noise2D(0.2 * absoluteX, 0.2 * absoluteZ));
 
+      for (let absoluteY = 0; absoluteY < height; absoluteY++) {
+        blocks[relativeX][absoluteY][relativeZ] = new BlockData({
+          id: `${absoluteX}|${absoluteY}|${absoluteZ}`,
+          position: [absoluteX, absoluteY, absoluteZ],
+          blockType: 1,
+        });
+      }
     }
   }
+  
+  return new BlockChunkData({id: `${x}|${z}`, blocks, position: [x, z]})
 }
+
+module.exports = {
+  genChunk,
+};

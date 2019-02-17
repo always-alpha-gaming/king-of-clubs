054f4e080358f623ad9a338e4abe72247c727f3c
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-02-17 04:56:22 -0500

    Fixed the DIsconnect on the Server

--- packages/server/server.js
+++ packages/server/server.js
@@ -8,12 +8,13 @@ gameLoop();
 // Handle The Client Connection
 io.on('connection', (socket) => {
   clientManager.onConnected(socket);
-});
 
-// Handle The Client Disconnect
-io.on('disconnect', (socket) => {
-  clientManager.onDisconnect(socket);
+  // Handle The Client Disconnect
+  socket.once('disconnect', function() {
+    clientManager.onDisconnect(socket);
+  });
 });
 
 // Start the Server and Listen on Port 3000
 io.listen(3000);
+console.log("Server listening on Port 3000 | http://localhost:3000/");

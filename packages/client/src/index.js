8288d9919bd72afc6a177b66dd8d61840321a8f0
Patrick Gingras <775.pg.12@gmail.com>
2019-02-16 23:33:57 -0500

    COLO~~U~~R

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,8 +1,13 @@
 import MainLoop from 'mainloop.js';
 import 'aframe';
 
-import { EVENTS, SKYBOX_COLOUR } from 'config';
-import { $, connect, waitFor } from './utilities';
+import { EVENTS, SKYBOX_COLOR, TEAMS } from 'config';
+import {
+  $,
+  connect,
+  createElement,
+  waitFor,
+} from './utilities';
 import World from './World';
 
 async function go() {
@@ -10,7 +15,7 @@ async function go() {
 
   // Set the Skybox Color to the CONST
   const skybox = $('a-sky');
-  skybox.setAttribute('color', SKYBOX_COLOUR);
+  skybox.setAttribute('color', SKYBOX_COLOR);
 
   // Start the Connection
   const connection = await connect(
--- packages/game-objects/src/Models/BlockData.js
+++ packages/game-objects/src/Models/BlockData.js
@@ -2,10 +2,10 @@ const CONFIG = require('config');
 
 class BlockData {
   /**
-   * Block Data Constructor 
+   * Block Data Constructor
    * @param {Object} param0 Configuration object containing the following...
    * @param {Vector3} param0.position The Absolute Position of the Block
-   * @param {Color?} param0.color A Named or Hex Colour
+   * @param {Color?} param0.color A Named or Hex Color
    * @param {Number} param0.health The Health of this block
    * @param {Number} param0.maxHealth The Maximum Health of this block
    * @param {Number} param0.blockType The Block Type CONFIG Index of this Block
@@ -18,7 +18,7 @@ class BlockData {
 
     // Color
     if (typeof color !== 'string') {
-      this.color = CONFIG.BLOCK_TYPES[blockType].colour;
+      this.color = CONFIG.BLOCK_TYPES[blockType].color;
     } else {
       this.color = color;
     }

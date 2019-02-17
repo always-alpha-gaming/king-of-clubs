c6f62de15f1a97aac9c1e889eb32a7c19168a710
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-02-16 21:08:39 -0500

    Changing the Skybox Color to the Const

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,12 +1,18 @@
 import MainLoop from 'mainloop.js';
 import 'aframe';
 
-import { EVENTS } from 'config';
+import { EVENTS, SKYBOX_COLOUR } from 'config';
 import { $, connect, waitFor } from './utilities';
 import World from './World';
 
 async function go() {
   const scene = $('a-scene');
+  
+  // Set the Skybox Color to the CONST
+  const skybox = $('a-sky');
+  skybox.color = SKYBOX_COLOUR;
+
+  // Start the Connection
   const connection = await connect(
     `${window.location}`.includes('localhost') ? 'http://localhost:3000' : '/',
   );

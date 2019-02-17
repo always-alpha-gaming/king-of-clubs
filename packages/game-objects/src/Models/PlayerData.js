1a504148cc66ecea64a6aa5fb8668b33066214cd
Patrick Gingras <775.pg.12@gmail.com>
2019-02-17 01:17:30 -0500

    update some config stuff

--- packages/game-objects/src/Models/PlayerData.js
+++ packages/game-objects/src/Models/PlayerData.js
@@ -1,20 +1,26 @@
-const { PLAYER } = require('config');
+const { PLAYER, TEAMS } = require('config');
 
 class PlayerData {
   /**
    * Player Data Constructor
    * @param {Object} param0 Configuration object containing the following...
    * @param {String} param0.id The Globally Unique ID of the Player
-   * @param {Vector3} param0.position The Absolute Postion of the Player 
-   * @param {Vector3} param0.rotation The Absolute Postion of the Player 
+   * @param {Vector3} param0.position The Absolute Postion of the Player
+   * @param {Vector3} param0.rotation The Absolute Postion of the Player
    * @param {Object} param0.teamIndex The Team CONFIG Index of this Team
    * @param {Number} param0.health The Health of this block
    */
-  constructor({id, position, rotation, teamIndex, health = null }) {
+  constructor({
+    id,
+    position,
+    rotation,
+    teamIndex,
+    health = null,
+  }) {
     this.id = id;
     this.position = position;
     this.rotation = rotation;
-    this.team = CONFIG.TEAMS[teamIndex];
+    this.team = TEAMS[teamIndex];
 
     this.width = PLAYER.BASE_SIZE;
     this.depth = PLAYER.BASE_SIZE;
@@ -22,7 +28,7 @@ class PlayerData {
 
     // Health
     if (typeof health !== 'number') {
-      this.health = CONFIG.PLAYER.MAX_HEALTH;
+      this.health = PLAYER.MAX_HEALTH;
     } else {
       this.health = health;
     }

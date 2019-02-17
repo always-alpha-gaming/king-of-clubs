bb16f3122e4bcb0fbd775fb35dec046b6c6e9dfe
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-02-17 03:46:35 -0500

    Implemented Fixed TimeStep in the server

--- packages/server/Game/gameLoop.js
+++ packages/server/Game/gameLoop.js
@@ -1,5 +1,11 @@
+const CONFIG = require('config');
 const MainLoop = require('mainloop.js');
+const clientManager = require('./Managers/ClientManager');
 const gameState = require('./Models/GameState');
+
+// Game Loop Fixed TimeStep
+const FIXED_TIME_STEP = 100;
+let currentTimeStep = 0;
 
 /**
  * Begins the MainLoop
@@ -14,7 +20,14 @@ function beginMainLoop() {
  * @param {number} deltaTime The Delta Time between Frames 
  */
 function update(deltaTime) {
+    // Determine if a fixed amount of time has passed on our server before we continue our loop.
+    // If we haven't passsed the FIXED_TIME_STEP, then leave the Update Loop
+    // When we have, subtract the FIXED_TIME_STEP from the currentTimeStep
+    currentTimeStep += deltaTime;
+    if (currentTimeStep < FIXED_TIME_STEP) return;
+    currentTimeStep -= FIXED_TIME_STEP;
 
+    // Now that we are updating on a fixed interval, begin the Server-Side GameLoop
 };
 
 module.exports = beginMainLoop;

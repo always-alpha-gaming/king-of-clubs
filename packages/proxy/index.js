151888b54306738c51aac396b71412e4d94b7c1b
macdja38 <jakeincanada@icloud.com>
2019-02-16 19:17:34 -0500

    Proxy WS server as well

--- packages/proxy/index.js
+++ packages/proxy/index.js
@@ -2,5 +2,6 @@ const proxy = require('express-http-proxy');
 const app = require('express')();
 
 app.use('/', proxy('localhost:1234'));
+app.use('/socket', proxy('localhost:3000'));
 
 app.listen(2000);

3aace5963ff19c2e4277d5a2175f9a093ca6524f
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-02-16 19:13:53 -0500

    Added Max Health to the Config

--- packages/game-objects/src/BlockData.js
+++ packages/game-objects/src/BlockData.js
@@ -7,18 +7,28 @@ class BlockData {
    * @param {Vector3} param0.position The Absolute Position of the Block
    * @param {Color?} param0.color A Named or Hex Colour
    * @param {Number} param0.health The Health of this block
+   * @param {Number} param0.maxHealth The Maximum Health of this block
    * @param {Object} param0.blockType The Block Type of this Block
    */
-  constructor({ id, position, color = null, health, blockType = 0 }) {
+  constructor({ id, position, color = null, health, maxHealth, blockType = 0 }) {
+    this.id = id;
+    this.position = position;
+    this.color = color;
+    this.blockType = blockType;
+
+    // Health
     if (typeof health !== 'number') {
       this.health = CONFIG.BLOCK_TYPES[blockType].health
     } else {
       this.health = health;
     }
-    this.id = id;
-    this.position = position;
-    this.color = color;
-    this.blockType = blockType;
+
+    // Max Health
+    if (typeof maxHealth !== 'number') {
+      this.maxHealth = CONFIG.BLOCK_TYPES[blockType].health
+    } else {
+      this.maxHealth = maxHealth;
+    }
   }
 
   update() {}

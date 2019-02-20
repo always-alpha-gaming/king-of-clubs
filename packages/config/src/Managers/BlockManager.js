bc126571780f766958585d97365f63f5a4f62638
Andrew Godfroy <andrew.godfroy@killerrin.com>
2019-02-20 10:16:05 -0500

    Updated Block Manager with more helpers

--- packages/config/src/Managers/BlockManager.js
+++ packages/config/src/Managers/BlockManager.js
@@ -1,4 +1,4 @@
-const CONFIG = require('config');
+const CONFIG = require('../../config.json');
 
 class BlockManager {
   constructor() {}
@@ -7,7 +7,20 @@ class BlockManager {
     return CONFIG.BLOCK_TYPES[id];
   }
 
+  getBlockTypeByName(name) {
+    for (let i = 0; i < this.getNumberBlockTypes(); i += 1) {
+      if (CONFIG.BLOCK_TYPES[i] === name) { return CONFIG.BLOCK_TYPES[i]; }
+    }
+    return null;
+  }
+
+  getNumberBlockTypes() { return CONFIG.BLOCK_TYPES.length; }
+
   getBlockSize() { return CONFIG.BLOCK_SIZE; }
+
+  getAirBLock() { return this.getBlockTypeByID(0); }
+
+  getUnknownBLock() { return this.getBlockTypeByID(1); }
 }
 
 // Export an instance of the class
